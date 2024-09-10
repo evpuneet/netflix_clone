74e233a9df97a5168345d9b1be144af59bc37685
Puneet <puneetkfl147@gmail.com>
2024-09-10 12:48:14 +0530

    done l2 commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< HEAD
 const nextConfig = {
     eslint:{
         ignoreDuringBuilds:true,
     }
 };
+=======
+const nextConfig = {};
+>>>>>>> afa098d (Initial commit from Create Next App)
 
 export default nextConfig;
